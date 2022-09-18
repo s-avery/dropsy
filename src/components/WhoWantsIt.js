7202aee8ff7356332837135bf9e839276be625cf
s-avery <s_avery@riseup.net>
2022-09-18 13:33:52 -0400

    got radio update character gear wanted half working

--- src/components/WhoWantsIt.js
+++ src/components/WhoWantsIt.js
@@ -13,6 +13,7 @@ import { useEffect, useState } from "react";
                         - would mean downloading the entire firebase in batch and then setting it to update with the new values but that's probably fine?
                          */
 const WhoWantsIt = ({
+	//#region props
 	characterList,
 	setCharacterList,
 	earring,
@@ -33,6 +34,7 @@ const WhoWantsIt = ({
 	setLegs,
 	feet,
 	setFeet,
+	//#endregion
 }) => {
 	// !STATE ZONE
 	const [droppedGearState, setDroppedGearState] = useState([]);
@@ -40,15 +42,8 @@ const WhoWantsIt = ({
 	// !COMPONENT MOUNT
 	useEffect(() => {}, []);
 
-	// for each drop, .map to see:
-	// droppedGearArray.map((drop) => {
-	// 	// if it DID drop:
-	// 	return drop > 0 ? "test" : "";
-	// });
-	// print an h3 of the gearpiece's name
-	// if a character wants it:
-	// print radio options for that character
-
+	// !LOGIC ZONE
+	// *array of objects tracking which gear dropped
 	let droppedGearArray = [
 		{ earring },
 		{ necklace },
@@ -61,10 +56,38 @@ const WhoWantsIt = ({
 		{ feet },
 	];
 
+	// *statless charlist to work with
 	let statelessCharacterList = [...characterList];
 
+	// *onChange for selecting a player to give a drop to
+	const giveDrop = (e, drop) => {
+		// loop thru character list to find matching character name (key)
+		statelessCharacterList.forEach((character) => {
+			// if character's name matches selected radio value:
+			if (character.key === e.target.value) {
+				// find matching drop and update its value to "got it"
+				// loop thru that character's gearPieces in gearPiecesObject
+				character.gearListItems.gearPiecesObject.gearPieces.forEach(
+					(gearPiece) => {
+						// if its pieceName is the same as e.target.name (this fn can't see dropName but they're equated below):
+						if (gearPiece.pieceName === e.target.name) {
+							// edit that char to update the drop's value for that char to "got it"
+							gearPiece.wanted = "got it";
+							console.log(character.key);
+							console.log(
+								character.gearListItems.gearPiecesObject
+									.gearPieces
+								// TODO this may need to be done on submit: as is if they click on 2 ppl it will change both. hmmm. is there a "do if checked"? use state? idk
+							);
+						}
+					}
+				);
+			}
+		});
+	};
+
 	return (
-		<>
+		<form className="drops__choices">
 			{
 				// for each drop...
 				droppedGearArray.map((drop) => {
@@ -77,7 +100,7 @@ const WhoWantsIt = ({
 					// print it if it has dropped
 					if (dropValue > 0) {
 						return (
-							<form className="drops__choices">
+							<>
 								<div className="drops__option">
 									<h3>{dropName}</h3>
 
@@ -90,6 +113,7 @@ const WhoWantsIt = ({
 													name={dropName}
 													value={character.key}
 													id={`${character.key}_${dropName}`}
+													onChange={giveDrop}
 													required
 												/>
 												<label
@@ -103,12 +127,17 @@ const WhoWantsIt = ({
 										);
 									})}
 								</div>
-							</form>
+							</>
 						);
 					}
 				})
 			}
-		</>
+			<div className="drops__submit">
+				<button className="button" type="submit">
+					submit!
+				</button>
+			</div>
+		</form>
 	);
 };
 
