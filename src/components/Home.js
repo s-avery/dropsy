97dd5ae668ca96d4fd3ad453c6709d856a90ecba
s-avery <s_avery@riseup.net>
2023-05-12 20:30:22 -0400

    preliminary intro text

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,10 +15,17 @@ const Home = () => {
                 <Link to="/charSelect" className="button">
                     view characters
                 </Link>
-
                 <Link to="/bossKilled" className="button">
                     we killed a boss!
                 </Link>
+                <p className="about">
+                    this is a tool to track which members of a group need a
+                    given piece of dropped raid gear.
+                </p>
+                <p className="about">
+                    after you create a profile for each character, dropsy will
+                    track who needs drops week to week. spreadsheets begone!
+                </p>
             </main>
         </div>
     );
