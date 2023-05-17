75a8e0fbe3e9a64bbd292a05e4332281646721c9
s-avery <s_avery@riseup.net>
2023-05-17 15:33:54 -0400

    comment out unused vars for main deploy purposes

--- src/App.js
+++ src/App.js
@@ -54,33 +54,33 @@ function App() {
     const [legs, setLegs] = useState(0);
     const [feet, setFeet] = useState(0);
 
-    const [droppedGear, setDroppedGear] = useState({
-        earring: 0,
-        necklace: 0,
-        bracelet: 0,
-        ring: 0,
-        weapon: 0,
-        head: 0,
-        body: 0,
-        hands: 0,
-        legs: 0,
-        feet: 0,
-    });
+    // const [droppedGear, setDroppedGear] = useState({
+    //     earring: 0,
+    //     necklace: 0,
+    //     bracelet: 0,
+    //     ring: 0,
+    //     weapon: 0,
+    //     head: 0,
+    //     body: 0,
+    //     hands: 0,
+    //     legs: 0,
+    //     feet: 0,
+    // });
 
-    const incrementDrops = (droppedGearPiece) =>
-        setDroppedGear((currentDroppedGear) => ({
-            ...currentDroppedGear,
-            [droppedGearPiece]: currentDroppedGear[droppedGearPiece] + 1,
-        }));
+    // const incrementDrops = (droppedGearPiece) =>
+    //     setDroppedGear((currentDroppedGear) => ({
+    //         ...currentDroppedGear,
+    //         [droppedGearPiece]: currentDroppedGear[droppedGearPiece] + 1,
+    //     }));
 
-    const decrementDrops = (droppedGearPiece) =>
-        setDroppedGear((currentDroppedGear) => ({
-            ...currentDroppedGear,
-            [droppedGearPiece]:
-                currentDroppedGear[droppedGearPiece] <= 0
-                    ? currentDroppedGear[droppedGearPiece]
-                    : currentDroppedGear[droppedGearPiece] - 1,
-        }));
+    // const decrementDrops = (droppedGearPiece) =>
+    //     setDroppedGear((currentDroppedGear) => ({
+    //         ...currentDroppedGear,
+    //         [droppedGearPiece]:
+    //             currentDroppedGear[droppedGearPiece] <= 0
+    //                 ? currentDroppedGear[droppedGearPiece]
+    //                 : currentDroppedGear[droppedGearPiece] - 1,
+    //     }));
 
     // !COMPONENT MOUNT
     // *define gearlist
